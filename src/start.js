7ee207d9c188e3dc894b43a69c5154520479badc
elberel0626-cloud <elberel0626@gmail.com>
2026-08-20 12:38:25 -0500

    Fix PO runtime account initialization crash

--- src/start.js
+++ src/start.js
@@ -1,4 +1,5 @@
 import { execFileSync } from 'node:child_process';
+import { readFile, writeFile } from 'node:fs/promises';
 import { fileURLToPath } from 'node:url';
 import { glAccounts, arDocuments, apDocuments } from './data/seed.js';
 import { applyStatementClassification } from './accountStatementClassification.js';
@@ -8,11 +9,26 @@ import { prepareIncomingPurchaseOrderWorkflowServer } from './incomingPurchaseOr
 import { preparePurchaseOrderPreferencesServer } from './purchaseOrderPreferencesPatch.js';
 import { preparePurchaseOrderReportingServer } from './purchaseOrderReportingPatch.js';
 
+async function makePoPreferencesRuntimeInitializationSafe(modulePath){
+  const moduleUrl=new URL(modulePath,import.meta.url);
+  const moduleFile=fileURLToPath(moduleUrl);
+  const source=await readFile(moduleFile,'utf8');
+  const postingAccountsIndex=source.indexOf('const POSTING_ACCOUNTS=');
+  if(postingAccountsIndex<0)throw new Error('Generated PO runtime is missing POSTING_ACCOUNTS initialization.');
+  const beforePostingAccounts=source.slice(0,postingAccountsIndex);
+  const safeBeforePostingAccounts=beforePostingAccounts.replaceAll('POSTING_ACCOUNTS.poRni',"'2020'");
+  if(safeBeforePostingAccounts!==beforePostingAccounts){
+    await writeFile(moduleFile,safeBeforePostingAccounts+source.slice(postingAccountsIndex),'utf8');
+  }
+  return modulePath;
+}
+
 normalizeSampleUnreleasedDocuments({ arDocuments, apDocuments });
 applyStatementClassification(glAccounts);
 const cashPurchaseServerModule = await prepareCashPurchaseApplicationServer();
 const incomingPoServerModule = await prepareIncomingPurchaseOrderWorkflowServer(cashPurchaseServerModule);
 const poPreferencesServerModule = await preparePurchaseOrderPreferencesServer(incomingPoServerModule);
+await makePoPreferencesRuntimeInitializationSafe(poPreferencesServerModule);
 const poReportingServerModule = await preparePurchaseOrderReportingServer(poPreferencesServerModule);
 const poReportingServerUrl = new URL(poReportingServerModule, import.meta.url);
 execFileSync(process.execPath, ['--check', fileURLToPath(poReportingServerUrl)], { stdio: 'inherit' });
